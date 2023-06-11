fba70ec52c8035a9f61848b0a24a6d12fc7a49be
Denis Efimov <defimov9@gmail.com>
2023-06-11 15:55:31 +0500

    feat: add user and card models

---
+++ models/card.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      default: {},
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
